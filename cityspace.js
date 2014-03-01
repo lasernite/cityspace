5a28f770deeb6d7e9295a17231208242af2f5796
Laser Nite <lasernite@gmail.com>
2014-03-01 18:15:46 -0500

    sid needs push

--- cityspace.js
+++ cityspace.js
@@ -10,6 +10,25 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
+
+  alert("AHHH ERORRO")
+  
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : "552958418145577",
+        status     : true,
+        xfbml      : true
+      });
+    };
+    
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/all.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
 }
 
 if (Meteor.isServer) {
